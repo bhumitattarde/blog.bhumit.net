34ab54100c2df4e626fc5d37912c6c571502f989
Bhumit Attarde <bhumit.attarde01@gmail.com>
2021-05-25 22:20:55 +0530

    Improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,51 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+import * as React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/js/Layout"
-import Seo from "../components/js/Layout"
+import Layout from "../components/js/Layout";
+import SEO from "../components/js/SEO";
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+import * as NotFoundPageStyle from "./css/404.module.css";
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-    </Layout>
-  )
+class NotFoundPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+
+    // member vars
+    this.data = this.props.data;
+    this.siteTitle = this.data.site.siteMetadata.title;
+    this.counterFnID = "";
+
+    // `this` bindingss
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  incrementCount() {
+    if (this.state.count < 404) {
+      this.setState((prevState) => {
+        return { count: prevState.count + 101 };
+      });
+    } else {
+      clearInterval(this.counterFnID);
+    }
+  }
+
+  componentDidMount() {
+    this.counterFnID = setInterval(this.incrementCount, 1000);
+  }
+
+  render() {
+    return (
+      <Layout location={this.props.location} title={this.siteTitle}>
+        <SEO title="404: Not Found" />
+        <h1>{this.state.count}</h1>
+        <h1>Nothing to see here..</h1>
+      </Layout>
+    );
+  }
 }
 
-export default NotFoundPage
+export default NotFoundPage;
 
 export const pageQuery = graphql`
   query {
@@ -25,4 +55,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
